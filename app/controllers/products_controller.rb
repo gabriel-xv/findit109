3fe497463ae674dca1b6ff162b43ab49d1889311
gabriel pacheco <gabrielagustinpacheco@gmail.com>
2020-11-19 12:32:30 +0100

    user can create a new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,28 @@
 class ProductsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @new_product = Product.new
+  end
+
+  def create
+    @new_product = Product.new(product_params)
+    @new_product.user = current_user
+
+    if @new_product.save
+      redirect_to product_path(@new_product)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :condition, :photo)
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user
-  has_many_attached :photos
+  has_one_attached :photo
 
   CONDITION = ['Excellent', 'Good', 'Ok', 'Bad'].freeze
 end
